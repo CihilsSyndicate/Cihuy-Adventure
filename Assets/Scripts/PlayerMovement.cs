a1b32154c2bc3ff2e6e31666f4473b2f3f788127
farid331 <faridsyahfadillah@gmail.com>
2023-09-22 16:58:41 +0700

    + Walk Anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    private Animator anim;
+    private Rigidbody2D myRb;
+    private Vector3 change;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        myRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
-        transform.Translate(movement);
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+        
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveChar();
+            anim.SetFloat("x", change.x);
+            anim.SetFloat("y", change.y);
+            anim.SetBool("Idle", false);
+        }
+        else
+            anim.SetBool("Idle", true);
+    }
+
+    void MoveChar()
+    {
+        myRb.MovePosition(
+            transform.position + change.normalized * speed * Time.deltaTime
+            );
     }
 }
