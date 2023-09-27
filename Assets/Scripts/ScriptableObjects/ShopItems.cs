a37bc20ac25c0b4a9c4aec0ce85338c102fe4eef
fariezzz <145751557+fariezzz@users.noreply.github.com>
2023-09-27 17:14:50 +0300

    Shop Items

---
+++ Assets/Scripts/ScriptableObjects/ShopItems.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShopMenu", menuName = "New Shop Item", order = 1)]
+public class ShopItems : ScriptableObject
+{
+    public string itemName;
+    public int cost;
+}
