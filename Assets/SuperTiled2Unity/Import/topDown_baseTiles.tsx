404938094b2f141895cdccd709a353a3fd66767f
farid331 <faridsyahfadillah@gmail.com>
2023-10-04 09:35:40 +0700

    test

--- Assets/SuperTiled2Unity/Import/topDown_baseTiles.tsx
+++ Assets/SuperTiled2Unity/Import/topDown_baseTiles.tsx
@@ -1,4 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="topDown_baseTiles" tilewidth="16" tileheight="16" tilecount="2112" columns="33">
- <image source="../../../../../../Users/muham/Downloads/raou_free_top_down_adventure/topDown_baseTiles.png" width="528" height="1024"/>
+<tileset version="1.10" tiledversion="1.10.1" name="topDown_baseTiles" tilewidth="16" tileheight="16" tilecount="2112" columns="33">
+ <image source="topDown_baseTiles.png" width="528" height="1024"/>
+ <wangsets>
+  <wangset name="p" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="138" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="139" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="140" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="171" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="172" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="173" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="174" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="175" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="204" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="205" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="206" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="207" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="208" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
 </tileset>
