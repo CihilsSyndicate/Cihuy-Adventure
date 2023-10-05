30be6a889ad49bbc45f07946edabd9a897c1039f
Tougashiro <muhammadadryan505@gmail.com>
2023-10-05 14:16:51 +0700

    Interior Home

---
+++ Assets/SuperTiled2Unity/Import/SmallItems.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SmallItems" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../Sprites/UI/InteriorHome/TopDownHouse_SmallItems.png" width="128" height="128"/>
+</tileset>
