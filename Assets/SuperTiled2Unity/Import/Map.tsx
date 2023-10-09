267ad1883db2d86268b3b66140aa2d7684853d2a
Tougashiro <muhammadadryan505@gmail.com>
2023-10-09 13:01:50 +0700

    ppppp

--- Assets/SuperTiled2Unity/Import/Map.tsx
+++ Assets/SuperTiled2Unity/Import/Map.tsx
@@ -38,6 +38,7 @@
    <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
    <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
    <wangcolor name="" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00e9ff" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="0,0,0,2,0,0,0,0"/>
    <wangtile tileid="1" wangid="0,0,0,2,0,2,0,0"/>
    <wangtile tileid="2" wangid="0,0,0,0,0,2,0,0"/>
