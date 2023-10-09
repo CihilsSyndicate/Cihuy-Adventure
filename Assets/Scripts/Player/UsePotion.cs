fd291051171781b9ed4d9e8519016269a9fe94a5
fariezzz <145751557+fariezzz@users.noreply.github.com>
2023-10-09 14:45:37 +0300

    owh

--- Assets/Scripts/Player/UsePotion.cs
+++ Assets/Scripts/Player/UsePotion.cs
@@ -7,17 +7,20 @@ public class UsePotion : MonoBehaviour
     public Image buttonImage; // Komponen Image pada tombol
     public Text quantityText; // Komponen Text (opsional) untuk menampilkan jumlah potion yang tersisa
 
-    private PlayerInventory playerInventory;
+    public PlayerInventory playerInventory;
 
     private void Start()
     {
-        playerInventory = FindObjectOfType<PlayerInventory>(); // Cari objek PlayerInventory di scene
-
-        // Set gambar dan teks tombol sesuai dengan item potion
-        if (potionItem != null)
+        if (potionItem != null && playerInventory.myInventory.Contains(potionItem))
         {
             buttonImage.sprite = potionItem.itemImage;
             UpdateButtonState();
+        }
+        else
+        {
+            // Jika potionItem tidak ada di dalam inventory, nonaktifkan tombol dan teksnya
+            buttonImage.gameObject.SetActive(false);
+            quantityText.gameObject.SetActive(false);
         }
     }
 
@@ -26,20 +29,15 @@ public class UsePotion : MonoBehaviour
 
     }
 
-    // Method yang dipanggil saat tombol diklik
     public void UseThePotion()
     {
         if (potionItem != null && potionItem.numberHeld > 0 && PlayerMovement.Instance.currentHealth.RuntimeValue < PlayerMovement.Instance.currentHealth.initialValue)
         {
-            // Gunakan potion
             potionItem.Use();
-
-            // Perbarui tampilan tombol
             UpdateButtonState();
         }
     }
 
-    // Metode untuk mengaktifkan atau menonaktifkan tombol berdasarkan jumlah potion yang tersisa
     private void UpdateButtonState()
     {
         if (potionItem.numberHeld > 0)
