d5e4d321aa54752cdff92f8820bf89e8431263c5
farid331 <faridsyahfadillah@gmail.com>
2023-10-10 11:23:11 +0700

    save & load

---
+++ Assets/Scripts/Player/WeaponData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+[Serializable]
+public class WeaponData
+{
+    public InventoryItem currentItem;
+
+    public WeaponData(WeaponManager weaponManager)
+    {
+        currentItem = weaponManager.weapon;
+    }
+}
