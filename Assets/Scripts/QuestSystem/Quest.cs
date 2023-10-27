7d10dca532dcbfb4373756d58dfd2a3a315d41cf
fariezzz <cihilxd46@gmail.com>
2023-10-27 20:25:02 +0700

    njirlh

---
+++ Assets/Scripts/QuestSystem/Quest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    public enum QuestProgress
+    {
+        NOT_AVAILABLE, AVAILABLE, ACCEPTED, COMPLETE, DONE
+    }
+
+    public string title;
+    public int id;
+    public QuestProgress progress;
+    public string description;
+    public string hint;
+    public string congratulation;
+    public string summary;
+    public int nextQuest;
+
+    public string questObjective;
+    public int questObjectiveCount;
+    public int questObjectiveRequirement;
+
+    public int coinReward;
+    public string itemReward;
+}
---
+++ Assets/Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public static QuestManager questManager;
+
+    public List<Quest> questList = new List<Quest>();
+    public List<Quest> currentQuestList = new List<Quest>();
+
+    private void Awake()
+    {
+        if(questManager == null)
+        {
+            questManager = this;
+        }
+        else if(questManager != null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Add Item
+    public void QuestItem(string questObjective, int itemAmount)
+    {
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            if(currentQuestList[i].questObjective == questObjective && currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                currentQuestList[i].questObjectiveCount += itemAmount;
+            }
+
+            if(currentQuestList[i].questObjectiveCount >= currentQuestList[i].questObjectiveRequirement && currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                currentQuestList[i].progress = Quest.QuestProgress.COMPLETE;
+            }
+        }
+    }
+
+    // Remove Item
+
+    public bool RequestAvailableQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool RequestAcceptedQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool RequestCompleteQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.COMPLETE)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
